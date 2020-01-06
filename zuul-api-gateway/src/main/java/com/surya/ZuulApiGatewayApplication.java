7d5456fbb2ab0c7b7b67884221236e46e3f3e275
Suryakant.Mandal <Suryakant.Mandal@SEZ59CIBC.india.rsystems.com>
2020-01-06 19:03:02 +0530

    spring zuul api gateway

---
+++ zuul-api-gateway/src/main/java/com/surya/ZuulApiGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public com.surya.filter.PreFilter PreFilter() {
+		return new com.surya.filter.PreFilter();
+	}
+
+}
